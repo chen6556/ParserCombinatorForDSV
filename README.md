35daebab082c9194a69b79da8919c007a8959ef0
chen6556 <liuyuchen6556@163.com>
2023-10-23 16:43:44 +0800

    添加文件。

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ParserCombinator
-A demo of Parser Combinator
+This a demo of Parser Combinator of Meta-programming, and also a part of DSV(https://github.com/chen6556/DSV).
+
+这是一个元编程中解析器组合子的Demo,同时也是DSV(https://github.com/chen6556/DSV)的一部分。
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Parser.hpp"
+
+
+class Importer
+{
+private:
+    size_t count = 0;
+
+public:
+    void print()
+    {
+        std::cout << "Get " << count << " times." << std::endl;
+    }
+
+    void print(const std::string &str)
+    {
+        std::cout << "Get " << count++ << " : " << str << std::endl;
+    }
+
+    void print(const double value)
+    {
+        std::cout << "Get " << count++ << " : " << value << std::endl;
+    }
+};
+
+
+int main()
+{
+    Importer importer;
+    Action<void> print_a(&importer, &Importer::print);
+    Action<std::string> prints_a(&importer, &Importer::print);
+    Action<double> printd_a(&importer, &Importer::print);
+
+    auto key = confix_p(ch_p('\"'), (*anychar_p())[prints_a], ch_p('\"'));
+    auto value = key | float_p()[printd_a];
+    auto array = confix_p(ch_p('['), *((float_p()[printd_a] | key) >> *ch_p(' ') >> ch_p(',')) >> *ch_p(' ') >> !(float_p()[printd_a] | key), ch_p(']'))[print_a];
+
+    std::string_view text("[123, \"Hello\"]");
+    array(text);
+
+    return 0;
+}
